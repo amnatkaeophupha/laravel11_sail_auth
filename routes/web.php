96b2cf7ae1c3cc76c643ed845a6f27ebebd062e2
amnatkaeophupha <phuphakaeo@gmail.com>
2024-12-18 15:21:27 +0700

    Tempages singin signup and forgot

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/signin', function () {
+    return view('auth.signin');
+});
+
+Route::get('/signup', function () {
+    return view('auth.signup');
+});
+
+Route::get('/forgot-password', function () {
+    return view('auth.forgot-password');
+});
